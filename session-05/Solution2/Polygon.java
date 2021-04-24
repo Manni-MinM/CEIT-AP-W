bf52504b3705464afc5ee232c37166075c9435c3
manni-minm <mannimoghimi@gmail.com>
2021-04-24 09:45:06 +0430

    added Javadoc

--- session-05/Solution2/Polygon.java
+++ session-05/Solution2/Polygon.java
@@ -1,12 +1,20 @@
-// BWOTSHECHB
+// BWOTSHEWCHB
 
 import java.util.ArrayList ;
 import java.util.Collections ;
 
+/**
+ * A Class to hold information about Polygons
+ */
 public abstract class Polygon extends Shape {
 	// Fields
 	ArrayList<Integer> sides ;
 	// Constructor
+	/**
+	 * Creates Polygon Object
+	 *
+	 * @param sides Sides of the Polygon
+	 */
 	public Polygon(int... sides) {
 		this.sides = new ArrayList<Integer>() ;
 		for ( int side : sides )
@@ -14,13 +22,41 @@ public abstract class Polygon extends Shape {
 		Collections.sort(this.sides) ;
 	}
 	// Methods
+	/**
+	 * Returns the sides of the Polygon
+	 *
+	 * @return sides An ArrayList containing the sides of the Polygon
+	 */
 	public ArrayList<Integer> getSides() {
 		return sides ;
 	}
+	/**
+         * Returns the perimeter of the Polygon
+         *
+         * @return Perimeter of the Polygon
+         */
 	public abstract double calculatePerimeter() ;
+	/**
+         * Returns the area of the Polygon
+         *
+         * @return Area of the Polygon
+         */
 	public abstract double calculateArea() ;
+	/**
+         * Prints details about the Polygon
+         */
 	public abstract void draw() ;
+	/** Checks if two Polygon Objects are equal
+         *
+         * @param object Object to be checked
+         * @return True if Objects are the same otherwise returns False
+         */
 	public abstract boolean equals(Object object) ;
+	/**
+         * Converts Polygon Object to String format
+         *
+         * @return Polygon Object in String format
+         */
 	public abstract String toString() ; 
 }
 
--- session-05/Solution2/Shape.java
+++ session-05/Solution2/Shape.java
@@ -1,10 +1,36 @@
-// BWOTSHECHB
+// BWOTSHEWCHB
 
+/**
+ * A Class to hold information about Shapes
+ */
 public abstract class Shape {
 	// Methods
+	/**
+         * Returns the perimeter of the Shape
+         *
+         * @return Perimeter of the Shape
+         */
 	public abstract double calculatePerimeter() ;
+	/**
+         * Returns the area of the Shape
+         *
+         * @return Area of the Shape
+         */
 	public abstract double calculateArea() ;
+	/**
+         * Prints details about the Shape
+         */
 	public abstract void draw() ;
+	/** Checks if two Shape Objects are equal
+         *
+         * @param object Object to be checked
+         * @return True if Objects are the same otherwise returns False
+         */
 	public abstract boolean equals(Object object) ;
+	/**
+         * Converts Shape Object to String format
+         *
+         * @return Shape Object in String format
+         */
 	public abstract String toString() ; 
 }
